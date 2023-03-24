31ee53a6ac9fa7bd4ce6163065b9255f2913e227
Robert Kleffner <rob.kleffner@gmail.com>
2023-03-24 16:38:11 -0600

    Finish ch 9

--- rob/ch9/ch9.rkt
+++ rob/ch9/ch9.rkt
@@ -5,10 +5,13 @@
 ;; PREVIOUS CHAPTER UTILITIES
 ;; ===============================
 
+(define epsilon 1e-08)
+
 (declare-hyper revs)
 (declare-hyper alpha)
 (declare-hyper batch-size)
 (declare-hyper mu)
+(declare-hyper beta)
 
 ;; data-set for a simple, linear line in 2d
 (define line-xs
@@ -192,3 +195,57 @@
 ;; =============================
 ;; CURRENT CHAPTER
 ;; =============================
+
+(define rms-i
+  (λ (p) (list p (zeroes p))))
+
+(define rms-d
+  (λ (P) (ref P 0)))
+
+(define rms-u
+  (λ (P g)
+    (let ([r (smooth beta (ref P 1) (sqr g))])
+      (let ([alpha-hat (/ alpha (+ (sqrt r) epsilon))])
+        (list (- (ref P 0) (* alpha-hat g)) r)))))
+
+(define rms-gradient-descent
+  (gradient-descent rms-i rms-d rms-u))
+
+(define try-plane
+  (λ (a-gradient-descent a-revs an-alpha)
+    (with-hypers
+        ([revs a-revs]
+         [alpha an-alpha]
+         [batch-size 4])
+      (a-gradient-descent (sampling-obj (l2-loss plane) plane-xs plane-ys)
+                          (list (tensor 0.0 0.0) 0.0)))))
+
+(with-hypers
+    ([beta 0.9])
+  (try-plane rms-gradient-descent 3000 0.01))
+
+
+
+(define adam-i
+  (λ (p)
+    (let ([v (zeroes p)])
+      (let ([r v])
+        (list p v r)))))
+
+(define adam-d
+  (λ (P) (ref P 0)))
+
+(define adam-u
+  (λ (P g)
+    (let ([r (smooth beta (ref P 2) (sqr g))])
+      (let ([alpha-hat (/ alpha (+ (sqrt r) epsilon))]
+            [v (smooth mu (ref P 1) g)])
+        (list (- (ref P 0) (* alpha-hat v)) v r)))))
+
+(define adam-gradient-descent
+  (gradient-descent adam-i adam-d adam-u))
+
+(with-hypers
+    ([mu 0.85]
+     [beta 0.9])
+  (try-plane adam-gradient-descent 1500 0.01))
